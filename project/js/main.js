e997d1b97a1d7c1dfedfa123eb3e9db1f93fd1f3
Andrey Kholmanov Work <ankholmanov@yandex.ru>
2019-04-02 15:03:35 +0700

    доработка кода

--- project/js/main.js
+++ project/js/main.js
@@ -3,16 +3,19 @@ const products = [
     {title: 'Mouse', price: 20},
     {title: 'Keyboard', price: 48},
     {title: 'Gamepad', price: 63},
-    {title: 'Chair'}
+    {title: 'Chair', price: 200}
 ];
 
 
-const renderProduct = (title, price = 500) => {
+const renderProduct = (title, price, img = 'https://placehold.it/200x150') => {
     return `<div class="product-item">
-                <h3>${title}</h3>
-                <p>${price}</p>
-                <button type="button" class="add-to-cart">Добавить</button>
-            </div>`
+                <img src="${img}" alt="some img">
+                <div class="desc">
+                    <h3>${title}</h3>
+                    <p>${price} $</p>
+                    <button type="button" class="add-to-cart">Добавить</button>
+                </div>
+            </div>`;
 };
 
 const renderPage = list => {
