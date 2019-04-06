4f9d48fe5ceec973331cbc597b19a06c0bd2b39e
KholmanovAndrey Home <khav1985@gmail.com>
2019-04-06 11:26:22 +0700

    урок 3 - 1 задание

--- project/js/main.js
+++ project/js/main.js
@@ -90,28 +90,3 @@ class CartItem {
 
 let products = new ProductsList();
 let cart = new CartList();
-
-// const products = [
-//     {title: 'Notebook', price: 2000},
-//     {title: 'Mouse', price: 20},
-//     {title: 'Keyboard', price: 48},
-//     {title: 'Gamepad', price: 63},
-//     {title: 'Chair', price: 200}
-// ];
-//
-// const renderProduct = (title, price, img = 'https://placehold.it/200x150') => {
-//     return `<div class="product-item">
-//                 <img src="${img}" alt="some img">
-//                 <div class="desc">
-//                     <h3>${title}</h3>
-//                     <p>${price} $</p>
-//                     <button type="button" class="add-to-cart">Добавить</button>
-//                 </div>
-//             </div>`;
-// };
-//
-// const renderPage = list => {
-//     document.querySelector('.products').innerHTML = list.map(item => renderProduct(item.title, item.price)).join('');
-// };
-//
-// renderPage(products);
