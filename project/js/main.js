c913f07ddea7f4234262ad975d453859967c6cc9
KholmanovAndrey Home <khav1985@gmail.com>
2019-04-07 12:39:53 +0700

    урок 3 - полная домашка

--- project/js/main.js
+++ project/js/main.js
@@ -46,7 +46,9 @@ class Product {
                     <div class="desc">
                         <h3>${this.product_name}</h3>
                         <p>${this.price} $</p>
-                        <button class="add-to-cart" data-id="${this.id_product}">Купить</button>
+                        <button class="button add-to-cart" data-id="${this.id_product
+                                                            }" data-name="${this.product_name
+                                                            }" data-price="${this.price}">Купить</button>
                     </div>
                 </div>`
     }
@@ -54,37 +56,164 @@ class Product {
 
 class CartList {
     constructor(){
-        this.products = [];
+        this.goods = [];
+        this.amount = 0;
+        this.countGoods = 0;
         this.container = document.querySelector('.cart');
-        this.renderAll();
+        this.button = document.querySelector('.btn-cart');
+        this.containerGoods = document.querySelector('.cart__goods');
+        this.containerCommon = document.querySelector('.cart__common');
+        this.container.style.display = 'none';
+        this.init();
     }
-    addItem(){
-        // получаем данные через событие по клику на button и передаем в this.products,
-        // чтоб можно было вывести всю корзину
-        // а так же выводим в корзину данные продукта через this.renderItem();
-        this.renderItem(this.container, product);
+    init(){
+        this._openCart();
+        this._getBasket();
+        this._addToBasket();
+        this._deleteFromBasket();
     }
-    renderAll(){
-        // выводим в корзину данные из this.products через forEach и this.renderItem();
-        this.products.forEach(product => {
-            this.renderItem(this.container, product);
+    _openCart(){
+        this.button.addEventListener('click', () => {
+            if (this.container.style.display === 'none') {
+                this.container.style.display = 'block';
+            } else {
+                this.container.style.display = 'none';
+            }
+        })
+    }
+    _getBasket(){
+        fetch(`${API}/getBasket.json`)
+            .then(result => result.json())
+            .then(data => {
+                this.goods = data.contents;
+                this.amount = data.amount;
+                this.countGoods = data.countGoods;
+                this.renderAllProduct();
+                this.renderCommon();
+            })
+            .catch(error => {
+                console.log(error)
+            });
+    }
+    _addToBasket(){
+        document.querySelector('.products').addEventListener('click', e => {
+            if (e.target.classList.contains('add-to-cart')){
+                fetch(`${API}/addToBasket.json`)
+                    .then(result => result.json())
+                    .then(data => {
+                        if (data.result === 1) {
+                            let goodNew = true;
+                            for (let i = 0; i < this.goods.length; i++) {
+                                if (this.goods[i].id_product === +e.target.dataset.id) {
+                                    this.goods[i].quantity++;
+                                    goodNew = false;
+                                    break;
+                                }
+                            }
+                            if (goodNew) {
+                                this.goods.push({
+                                    id_product: +e.target.dataset.id,
+                                    product_name: e.target.dataset.name,
+                                    price: +e.target.dataset.price,
+                                    quantity: 1
+                                });
+                                this.renderItem(this.goods[this.goods.length - 1]);
+                            }
+                            this._calcOfAmount();
+                            this._calcOfCountCoods();
+                            this.renderCommon();
+                            this.renderAllProduct();
+                        }
+                    })
+                    .catch(error => {
+                        console.log(error)
+                    });
+            }
         });
     }
-    renderItem(container, product){
+    _deleteFromBasket(){
+        document.querySelector('.cart').addEventListener('click', e => {
+            if (e.target.classList.contains('cart-item-del')){
+                fetch(`${API}/addToBasket.json`)
+                    .then(result => result.json())
+                    .then(data => {
+                        if (data.result === 1) {
+                            for (let i = 0; i < this.goods.length; i++) {
+                                if (this.goods[i].id_product === +e.target.dataset.id) {
+                                    this.goods[i].quantity--;
+                                    if (this.goods[i].quantity === 0) {
+                                        this.goods.splice(i, 1);
+                                    }
+                                }
+                            }
+                            this._calcOfAmount();
+                            this._calcOfCountCoods();
+                            this.renderCommon();
+                            this.renderAllProduct();
+                        }
+                    })
+                    .catch(error => {
+                        console.log(error)
+                    });
+            }
+        });
+    }
+    _calcOfAmount(){
+        this.amount = 0;
+        this.goods.forEach(good => {
+            this.amount += good.price * good.quantity;
+        });
+    }
+    _calcOfCountCoods(){
+        this.countGoods = 0;
+        this.goods.forEach(good => {
+            this.countGoods += good.quantity;
+        });
+    }
+    renderAllProduct(){
+        this.containerGoods.innerHTML = '';
+        // выводим в корзину данные из this.products через forEach и this.renderItem();
+        this.goods.forEach(product => {
+            this.renderItem(product);
+        });
+    }
+    renderCommon(){
+        this.containerCommon.innerHTML = '';
+
+        const html = `<p>countGoods: ${this.countGoods}</p><p>Amount: ${this.amount}</p>`;
+
+        this.containerCommon.insertAdjacentHTML('beforeend', html);
+    }
+    renderItem(product){
         // выводим в корзину данные продукта через new CartItem(product).render();
         const item = new CartItem(product);
-        container.insertAdjacentHTML('beforeend', item.render());
+        this.containerGoods.insertAdjacentHTML('beforeend', item.render());
     }
 }
 
 class CartItem {
-    constructor(product){
-        this.title = product.title;
+    constructor(product, img = 'https://placehold.it/80x120'){
+        this.id_product = product.id_product;
+        this.product_name = product.product_name;
         this.price = product.price;
+        this.quantity = product.quantity;
         this.img = img;
     }
     render(){
-        return '<div class="cart-item"></div>';
+        return `<div class="cart-item" data-id="${this.id_product}">
+                    <img src="${this.img}" alt="Some img">
+                    <div class="cart-item__desc1">
+                        <h3>${this.product_name}</h3>
+                        <p><b>quantity: ${this.quantity}</b></p>
+                        <p>${this.price} $</p>
+                    </div>
+                    <div class="cart-item__desc2">
+                        <h3>${this.price * this.quantity} $</h3>
+                        <button class="button cart-item-del" data-id="${this.id_product
+                                                            }" data-name="${this.product_name
+                                                            }" data-price="${this.price}">x</button>
+                    </div>
+                </div>`;
     }
 }
 
