0e99840068591157940c5b22f687ea3165698d9f
KholmanovAndrey Home <khav1985@gmail.com>
2019-04-20 21:03:05 +0700

    домашка

--- project_hard/js/ProdComp.js
+++ project_hard/js/ProdComp.js
@@ -24,8 +24,8 @@ Vue.component('products', {
             });
     },
     methods: {
-        filter(){
-            let regexp = new RegExp(this.$root.$refs.search.userSearch, 'i');
+        filter(userSearch){
+            let regexp = new RegExp(userSearch, 'i');
             this.filtered = this.products.filter(el => regexp.test(el.product_name));
         }
     },
--- project_hard/js/SearchComp.js
+++ project_hard/js/SearchComp.js
@@ -4,7 +4,7 @@ Vue.component('search', {
             userSearch: '',
         }
     },
-    template: `<form action="#" class="search-form" @submit.parent="$parent.$refs.products.filter">
+    template: `<form action="#" class="search-form" @submit.prevent="$parent.$refs.products.filter(userSearch)">
                     <input type="text" class="search-field" v-model="userSearch">
                     <button class="btn-search" type="submit">
                         <i class="fas fa-search"></i>
